9243f93dd4dedbcd93165915a972222188758ab4
nmk098 <nkuryumov@gmail.com>
2023-03-17 16:01:54 +0400

    final add

--- sparrowAnimate/ViewController.swift
+++ sparrowAnimate/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var animView: UIView = UIView()
+    var animator: UIDynamicAnimator!
+    var attachmentBehavior: UIAttachmentBehavior!
+    var attachment: UIAttachmentBehavior!
+    var collision: UICollisionBehavior!
+    var snapBehavior: UISnapBehavior?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(named: "AccentColor")
+        view.addSubview(animView)
+        animView.frame = CGRect(x: view.bounds.midX, y: view.bounds.midY, width: 100, height: 100)
+        animView.backgroundColor = UIColor(named: "viewColor")
+        animView.layer.cornerRadius = 20
+        animView.translatesAutoresizingMaskIntoConstraints = false
+        animView.layer.borderWidth = 2
+        animView.layer.borderColor = .init(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+
+        collision = UICollisionBehavior(items: [animView])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        self.view.addGestureRecognizer(tapGesture)
+        animator.addBehavior(collision)
+
+
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer) {
+        let location = sender.location(in: self.view)
+        let snapBehavior = UISnapBehavior(item: animView, snapTo: location)
+            snapBehavior.damping = 1
+            animator.removeAllBehaviors()
+        animator.addBehavior(collision)
+            animator.addBehavior(snapBehavior)
+        }
     }
 
-
-}
 
